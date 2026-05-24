af41fcced817755cb0952ebe7683916d6fe15bd4
Suyash Srivastava <73711602+suy0x1@users.noreply.github.com>
2026-05-24 14:22:27 +0530

    feat(cli): add collector service

--- cli/src/cli/parser.rs
+++ cli/src/cli/parser.rs
@@ -8,6 +8,8 @@ use crate::{
 fn parse_service(service: &str) -> Result<Service> {
     match service {
         "logger" => Ok(Service::Logger),
+        
+        "proxy-collector" => Ok(Service::ProxyCollector),
 
         "dns" => Ok(Service::DNS),
 
--- cli/src/runtime/service.rs
+++ cli/src/runtime/service.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Service {
     Logger,
+    ProxyCollector,
     DNS,
     CacheCleaner,
     CachePreloader,
